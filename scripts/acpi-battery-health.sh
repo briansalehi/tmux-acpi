9c105362e3e7132244f6b1c3008497cb125767dd
Brian Salehi <salehibrian@gmail.com>
2023-11-18 11:24:01 +0330

    add space between icon and percentage to avoid overlap

--- scripts/acpi-battery-health.sh
+++ scripts/acpi-battery-health.sh
@@ -5,7 +5,7 @@ health_icon="$1"
 if command -v acpi &>/dev/null
 then
     health_output="$(acpi -bi | grep 'Battery 0' | grep -o '= [0-9]\+')"
-    echo "${health_icon}${health_output/= /}%"
+    echo "${health_icon} ${health_output/= /}%"
 else
     echo "ACPI Unavailable"
 fi
