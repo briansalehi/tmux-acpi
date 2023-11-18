33c1b62a76e55a10d11616fe2a2c0fc5db8c4723
Brian Salehi <salehibrian@gmail.com>
2023-11-18 11:45:03 +0330

    add space to icons and remove hard code space

--- scripts/acpi-battery-percentage.sh
+++ scripts/acpi-battery-percentage.sh
@@ -5,7 +5,7 @@ battery_icon="$1"
 if command -v acpi &>/dev/null
 then
     state="$(acpi -b | grep -o '[0-9]\+%')"
-    echo "${battery_icon} ${state}"
+    echo "${battery_icon}${state}"
 else
     echo "ACPI Unavailable"
 fi
