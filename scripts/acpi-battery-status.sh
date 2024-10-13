6fa017163c502e4957c79b210f4a8810af123b77
Brian Salehi <briansalehi@proton.me>
2024-10-13 13:00:44 +0200

    battery: prevent incorrect indicator when battery is fully charged

--- scripts/acpi-battery-status.sh
+++ scripts/acpi-battery-status.sh
@@ -5,12 +5,19 @@ then
     state="$(acpi -b | grep 'Battery 0')"
     state="${state#*: }"
     state="${state%%,*}"
+    percentage="$(acpi -b | grep -o '[0-9]\+%')"
 
     case "${state,,}" in
         "charging") echo "Charging" ;;
         "discharging") echo "Discharging" ;;
         "not charging") echo "Not Charging" ;;
-        *) echo "⚠️ No Battery"
+        *)
+            if [ ${percentage:-0} -eq 100 ]
+            then
+                echo "Not Charging"
+            else
+                echo "⚠️ No Battery"
+            fi ;;
     esac
 else
     echo "ACPI Unavailable"
